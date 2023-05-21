407eb214073aceaf854aee9d93cbcbd918fd3df0
Hideaki Fukami <denys.fukami@gmail.com>
2023-05-21 20:25:29 -0300

    aula 12

---
+++ js/home.js
@@ -0,0 +1,7 @@
+$('.novidades button').click(function() {
+    $('.novidades').addClass('painel-aberto');
+});
+
+$('.mais-vendidos button').click(function() {
+    $('.mais-vendidos').addClass('painel-aberto');
+});
